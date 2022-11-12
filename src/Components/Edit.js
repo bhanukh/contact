a11020ce097d5ef7b346ca52b5b482820c5e312f
bhanukh <brkhandelwal1997@gmail.com>
2022-11-12 15:58:46 +0530

    first

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -4,40 +4,41 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify';
 
 const Edit = () => {
-     //state managment
-    const [name, setName]=useState('')
-    const [email, setEmail]=useState('')
-    const [phone, setPhone]=useState('')
-   
-    const {id} =useParams();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [number, setNumber] = useState('');
+
+    const { id } = useParams();
+
     const contacts = useSelector(state => state);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const currentContact = contacts.find(contact => contact.id === parseInt(id));
 
-    useEffect(()=>{
-        if(currentContact){
+    useEffect(() => {
+        if (currentContact) {
             setName(currentContact.name);
             setEmail(currentContact.email);
-            setPhone(currentContact.phone);
+            setNumber(currentContact.number);
         }
-    },[currentContact]);
+
+    }, [currentContact]);
 
     const handelSubmit = e => {
         e.preventDefault();
 
         const checkEmail = contacts.find(contact => contact.id !== parseInt(id) && contact.email === email);
-        const checkPhone = contacts.find(contact => contact.id !== parseInt(id) && contact.phone === parseInt(phone));
+        const checkNumber = contacts.find(contact => contact.id !== parseInt(id) && contact.number === parseInt(number));
 
-        if (!email || !phone || !name) {
-            return toast.warning("Please fill  all fields!");
+        if (!email || !number || !name) {
+            return toast.warning("Please fill in all fields!");
         }
 
         if (checkEmail) {
             return toast.error("This email already Exists!");
         }
 
-        if (checkPhone) {
+        if (checkNumber) {
             return toast.error("This number already Exists!");
         }
 
@@ -45,7 +46,7 @@ const Edit = () => {
             id: parseInt(id),
             name,
             email,
-            phone
+            number
         }
 
         dispatch({ type: 'UPDATE_CONTACT', payload: data });
@@ -53,43 +54,42 @@ const Edit = () => {
         navigate('/');
     };
 
+    return (
+        <div className='container'>
+            {
+                currentContact ? (
+                    <>
+                        <h1 className='display-3 text-center fw-bold'>Edit Contact {id}</h1>
+                        <div className='row'>
+                            <div className='col-md-6 shadow mx-auto p-5'>
+                                <form className='text-center' onSubmit={handelSubmit}>
+                                    <div className='form-group mb-3'>
+                                        <input type='text' placeholder='Name' className='form-control'
+                                            value={name} onChange={e => setName(e.target.value)} />
+                                    </div>
+                                    <div className='form-group mb-3'>
+                                        <input type='email' placeholder='Email' className='form-control'
+                                            value={email} onChange={e => setEmail(e.target.value)} />
+                                    </div>
+                                    <div className='form-group mb-3'>
+                                        <input type='number' placeholder='Phone Number' className='form-control'
+                                            value={number} onChange={e => setNumber(e.target.value)} />
+                                    </div>
+                                    <div className='form-group mb-3'>
+                                        <input type='submit' value='Update Contact' className='btn btn-dark' />
+                                        <Link to='/' className='btn btn-danger ms-3 '>Cancel</Link>
+                                    </div>
+                                </form>
+                            </div>
+                        </div >
+                    </>
+                ) : (
+                    <h1 className='display-3 my-5 text-center fw-bold'>Contact with id {id} does not exists!!</h1>
+                )
+            }
 
-  return (
-    <div className='container'>
-    {
-        currentContact ? (
-            <>
-                <h1 className='display-3 text-center fw-bold'>Edit Contact {id}</h1>
-                <div className='row'>
-                    <div className='col-md-6 shadow mx-auto p-5'>
-                        <form className='text-center' onSubmit={handelSubmit}>
-                            <div className='form-group mb-3'>
-                                <input type='text' placeholder='Name' className='form-control'
-                                    value={name} onChange={e => setName(e.target.value)} />
-                            </div>
-                            <div className='form-group mb-3'>
-                                <input type='email' placeholder='Email' className='form-control'
-                                    value={email} onChange={e => setEmail(e.target.value)} />
-                            </div>
-                            <div className='form-group mb-3'>
-                                <input type='number' placeholder='Phone Number' className='form-control'
-                                    value={phone} onChange={e => setPhone(e.target.value)} />
-                            </div>
-                            <div className='form-group mb-3'>
-                                <input type='submit' value='Update Contact' className='btn btn-dark' />
-                                <Link to='/' className='btn btn-danger ms-3 '>Cancel</Link>
-                            </div>
-                        </form>
-                    </div>
-                </div >
-            </>
-        ) : (
-            <h1 className='display-3 my-5 text-center fw-bold'>Contact with id {id} does not exists!!</h1>
-        )
-    }
-
-</div >
-)
+        </div >
+    )
 }
 
 export default Edit
